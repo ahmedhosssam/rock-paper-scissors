bc22cf2877816bcaf9e4e3a5437311cd5a6aaed1
Ahmed Hossam <hoss4614@gmail.com>
2022-11-15 19:30:12 +0200

    finish logic v1

--- main.js
+++ main.js
@@ -15,42 +15,57 @@ p = scissors :
   c = rock ==> lose
 */
 
-choices = ['rock', 'paper', 'scissors'];
+const choices = ['rock', 'paper', 'scissors'];
 
 const getComputerChoice = () => {
   return choices[Math.floor(Math.random() * choices.length)];
 };
 
-const whoWinner = (playerSelection) => {
+let playerScore = 0;
+let ComputerScore = 0;
+
+const play = (playerSelection) => {
   const computerSelection = getComputerChoice();
   const winner = `You Win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`;
   const loser = `You Lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`;
 
   if (playerSelection.toLowerCase() == 'rock') {
     if (computerSelection.toLowerCase() == 'scissors') {
-      return winner;
+      playerScore++;
+      console.log(winner);
     } else if (computerSelection.toLowerCase() == 'paper') {
-      return loser;
+      ComputerScore++;
+      console.log(loser);
     } else if (computerSelection.toLowerCase() == 'rock') {
-      return 'Draw!';
+      console.log('Draw!');
     }
   } else if (playerSelection.toLowerCase() == 'paper') {
     if (computerSelection.toLowerCase() == 'scissors') {
-      return loser;
+      ComputerScore++;
+      console.log(loser);
     } else if (computerSelection.toLowerCase() == 'paper') {
-      return `Draw!`;
+      console.log(`Draw!`);
     } else if (computerSelection.toLowerCase() == 'rock') {
-      return winner;
+      playerScore++;
+      console.log(winner);
     }
   } else if (playerSelection.toLowerCase() == 'scissors') {
     if (computerSelection.toLowerCase() == 'scissors') {
-      return `Draw!`;
+      console.log(`Draw!`);
     } else if (computerSelection.toLowerCase() == 'paper') {
-      return winner;
+      playerScore++;
+      console.log(winner);
     } else if (computerSelection.toLowerCase() == 'rock') {
-      return loser;
+      ComputerScore++;
+      console.log(loser);
     }
   }
 };
 
-console.log(whoWinner('paper'));
+for (let i = 0; i <= 5; i++) {
+  let choice = prompt('Rock , Paper , Scissors ?');
+
+  console.log(play(choice));
+}
+
+console.log(`Result : Player ${playerScore} - ${ComputerScore} Computer`);
