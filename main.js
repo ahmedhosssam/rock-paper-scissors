6fb4b78e169ac2594b3b52af68b97e381a36cb8f
ahmedhossam <hoss4614@gmail.com>
2022-11-17 20:44:34 +0200

    update result section

--- main.js
+++ main.js
@@ -30,6 +30,9 @@ const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const playerScorePara = document.getElementById('playerScore');
 const computerScorePara = document.getElementById('computerScore');
+const choose = document.getElementById('choose');
+const playerIcon = document.getElementById('playerIcon');
+const computerIcon = document.getElementById('computerIcon');
 
 const play = (playerSelection) => {
   const computerSelection = getComputerChoice();
@@ -38,36 +41,60 @@ const play = (playerSelection) => {
     if (computerSelection.toLowerCase() == 'scissors') {
       playerScore++;
       playerScorePara.textContent = `Player : ${playerScore}`;
+      choose.textContent = `You WON! ✊ beats ✌`;
+      playerIcon.textContent = `✊`;
+      computerIcon.textContent = `✌`;
       console.log('winner');
     } else if (computerSelection.toLowerCase() == 'paper') {
       ComputerScore++;
       computerScorePara.textContent = `Computer : ${ComputerScore}`;
+      choose.textContent = `You LOST! ✋ beats ✊`;
+      playerIcon.textContent = `✊`;
+      computerIcon.textContent = `✋`;
       console.log('loser');
     } else if (computerSelection.toLowerCase() == 'rock') {
+      choose.textContent = `Tie!`;
+      playerIcon.textContent = computerIcon.textContent = `✊`;
       console.log('Draw!');
     }
   } else if (playerSelection.id == 'paper') {
     if (computerSelection.toLowerCase() == 'scissors') {
       ComputerScore++;
       computerScorePara.textContent = `Computer : ${ComputerScore}`;
+      choose.textContent = `You LOST! ✌ beats ✋`;
+      playerIcon.textContent = `✋`;
+      computerIcon.textContent = `✌`;
       console.log('loser');
     } else if (computerSelection.toLowerCase() == 'paper') {
+      choose.textContent = `Tie!`;
+      playerIcon.textContent = computerIcon.textContent = `✋`;
       console.log(`Draw!`);
     } else if (computerSelection.toLowerCase() == 'rock') {
       playerScore++;
       playerScorePara.textContent = `Player : ${playerScore}`;
+      choose.textContent = `You WON ! ✋ beats ✊`;
+      playerIcon.textContent = `✋`;
+      computerIcon.textContent = `✊`;
       console.log('winner');
     }
   } else if (playerSelection.id == 'scissors') {
     if (computerSelection.toLowerCase() == 'scissors') {
+      choose.textContent = `Tie!`;
+      playerIcon.textContent = computerIcon.textContent = `✌`;
       console.log(`Draw!`);
     } else if (computerSelection.toLowerCase() == 'paper') {
       playerScore++;
       playerScorePara.textContent = `Player : ${playerScore}`;
+      choose.textContent = `You WON ! ✌ beats ✋`;
+      playerIcon.textContent = `✌`;
+      computerIcon.textContent = `✋`;
       console.log('winner');
     } else if (computerSelection.toLowerCase() == 'rock') {
       ComputerScore++;
       computerScorePara.textContent = `Computer : ${ComputerScore}`;
+      choose.textContent = `You LOST! ✊ beats ✌`;
+      playerIcon.textContent = `✌`;
+      computerIcon.textContent = `✊`;
       console.log('loser');
     }
   }
